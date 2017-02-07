2a32ce0e66feea377e421a86209b500f1609c0a2
Arild <helenbog@stud.ntnu.no>
2017-02-07 16:18:37 +0100

    create test program

--- main.go
+++ main.go
@@ -5,9 +5,57 @@ import (
 	. "fmt"
 )
 
+LIGHT_IND := {FLOOR_IND1, FLOOR_IND2, FLOOR_IND3, FLOOR_IND4}
+
+func checkSensors() int {
+	if driver.ReadBit(SENSOR1) {
+		return 1
+	}
+	if driver.ReadBit(SENSOR2) {
+		return 2
+	}
+	if driver.ReadBit(SENSOR3) {
+		return 3
+	}
+	if driver.ReadBit(SENSOR4) {
+		return 4
+	}
+	return -1
+}
+
 func main() {
 	Println("test")
-	for {
 
+	driver.io_clear_bit(MOTORDIR)
+	driver.WriteAnalog(MOTOR, 2800)
+	last := 0
+	for {
+		i := checkSensors()
+		switch i {
+			case last:
+				break
+			case -1: 
+				driver.clearBit(LIGHT_IND[last-1])
+			default:
+				driver.SetBit(LIGHT_IND[i-1])
+		}
+		last = i
+
+		if driver:ReadBit(OBSTRUCTION) {
+			driver.WriteAnalog(MOTOR, 0)
+		}
+		else{	
+			if driver.ReadBit(SENSOR1) {
+				//Drive up
+				driver.io_clear_bit(MOTORDIR)
+				driver.WriteAnalog(MOTOR, 2800)
+			}
+
+			if driver.ReadBit(SENSOR4) {
+				//Drive down
+				driver.io_clear_bit(MOTORDIR)
+				driver.WriteAnalog(MOTOR, 2800)
+			}
+		}
 	}
 }
